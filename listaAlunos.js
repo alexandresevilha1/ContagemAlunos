a10dcd6100e4846c4aec06e6911acc86e506157f
Alexandre <alexandre.sevilha1@gmail.com>
2021-10-18 18:40:26 -0300

    Contagem de alunos

---
+++ listaAlunos.js
@@ -0,0 +1,14 @@
+let ListaAlunos = 5;
+
+for(let contador = 0; contador <5; contador ++){
+
+
+
+if(contador == 0) {
+    console.log(contador + " :ZERO");
+} else if (contador % 2 == 1){
+    console.log(`${contador}: IMPAR`);
+} else {
+    console.log(`${contador}: PAR`)
+}
+}
